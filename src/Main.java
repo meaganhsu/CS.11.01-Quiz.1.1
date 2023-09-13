dcf8c121ed118d8deb7f46ffc59d9354426b5da3
meaganhsu <myth72@student.cis.edu.hk>
2023-09-13 09:24:45 +0800

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName = "";
+        String favouriteFood = "";
 
+        System.out.println("Please enter your age: ");
+        age = scanner.nextInt();
 
+        System.out.println("Please enter your name: ");
+        firstName = scanner.next();
 
+        System.out.println("Please enter your favourite food: ");
+        favouriteFood = scanner.next();
 
+        System.out.println("\nName: " + firstName + "\nAge: " + age + "\nFavourite food: " + favouriteFood);
     }
 
 }
